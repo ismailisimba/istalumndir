622ac085be8cc69c99d2f75c81ddea0730b8d2cd
ismizo <nerdyjinnyas@gmail.com>
2020-12-21 14:17:05 +0300

    search bar updates mond21

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 let localVar = {"cloudobject":{},
                 "searchcontext":"all",
-              "searchResults":[{}],
-            "lastRow":0};
+                "searchResults":[],
+                "searchKey":{  "type":"typeofkey",
+                               "value":"valueofkey",
+                               "textVal":"optionstext"},
+                "lastRow":0};
 
+let mySearchQuery = document.querySelectorAll(".mysearchquery")[0];
+let mySearchBut = document.getElementById("myfancysearchbut");
+mySearchQuery.innerHTML = "";
+
+let cardDiv = document.getElementById("childrencontainerone").cloneNode(true);
 
 let nodes = document.querySelectorAll(".childreninsidecontainer");
 let hiddenMenu = document.getElementById("detailedmenu");
@@ -49,6 +57,7 @@ function mainFunc () {
   // fetchInfoWithFilter ();
     myasync();
     searchBarStuff();
+    searchButStuff();
 
 
 
@@ -65,41 +74,25 @@ function addClickEventFunc(element,i) {
   
 
 
-        element.addEventListener("click", function (){
-
-          //  hiddenMenu.style.visibility === "visible";
-        
-
-                hiddenMenu.style.visibility = "visible";
-
-            
-               // title1.innerHTML = this.id;
-
-               
-
-               title1.innerHTML = localVar.cloudobject.busyobj[i].busyName;
-               description1.innerHTML = localVar.cloudobject.busyobj[i].busyDescription;
-               email.innerHTML = localVar.cloudobject.busyobj[i].email;
-               phoneone.innerHTML = localVar.cloudobject.busyobj[i].phoneOne;
-               phonetwo.innerHTML = localVar.cloudobject.busyobj[i].phoneTwo;
-               phonethree.innerHTML = localVar.cloudobject.busyobj[i].phoneThree;
-               map.innerHTML = localVar.cloudobject.busyobj[i].mapOne;
-               title2.innerHTML = localVar.cloudobject.busyobj[i].alumName;
-               description2.innerHTML = localVar.cloudobject.busyobj[i].alumBio;
-
-              
-
-              // description1.innerHTML = i;
-                
-
-            
-        });
+        element.addEventListener("click", cardClickFunction);
 
       
-
-
        addAnimation(element,i);
 
+
+
+}
+
+function removeClickEventFunc(element,i) {
+
+  
+  //  let styleTemp = window.getComputedStyle(hiddenMenu);
+  
+
+
+        element.removeEventListener("click", cardClickFunction, false);
+
+    
 
 
 }
@@ -163,13 +156,14 @@ async function myasync () {
 
   //hiddenMenu.innerHTML = localVar;
 
- let tempDiv = document.getElementById("childrencontainerone").cloneNode(true);
+ 
 
   motherDiv.innerHTML = "";
 
-  let boxes = localVar.cloudobject.lastRow;
+  
+  localVar.lastRow = localVar.cloudobject.lastRow;
   spanNumall[0].innerHTML = (localVar.cloudobject.lastRow-1);
-  boxes--;
+  
 
  // motherDiv.appendChild(tempclone);
 
@@ -185,7 +179,11 @@ async function myasync () {
     
   }*/
 
-  populatingBoxes (boxes, tempDiv);
+
+  let context = "all";
+  populateSearchResults(context);
+  
+  populatingBoxes (localVar.lastRow-1, cardDiv);
 
 
 
@@ -215,11 +213,13 @@ function addAnimation(element,i){
 }
 
 
-function populatingBoxes (boxes, tempDiv) {
+function populatingBoxes (boxes, myCardDiv) {
 
-  for (let i=1; i<boxes; i++){
+  motherDiv.innerHTML = "";
 
-   var  tempclone = tempDiv.cloneNode(true);
+  for (let i=0; i<boxes; i++){
+
+   var  tempclone = myCardDiv.cloneNode(true);
     tempclone.id = "childnumindex"+i;
 
     motherDiv.appendChild(tempclone);
@@ -228,16 +228,17 @@ function populatingBoxes (boxes, tempDiv) {
     let mainpicdiv = tempclone.querySelectorAll(".children:last-child");
     let busylogo = tempclone.querySelectorAll(".seconddeets");
     let catchphrasediv = tempclone.querySelectorAll(".catchphrase");
-    let bionamediv = tempclone.querySelectorAll(".profname");
-    let classofdiv = tempclone.querySelectorAll(".classOf");
+    //let bionamediv = tempclone.querySelectorAll(".profname");
+    //let classofdiv = tempclone.querySelectorAll(".classOf");
 
-    busytitlediv[0].innerHTML = localVar.cloudobject.busyobj[i].busyName;
-    mainpicdiv[0].style.backgroundImage = `url("${localVar.cloudobject.busyobj[i].mainImage}")`;
-    busylogo[0].style.backgroundImage = `url("${localVar.cloudobject.busyobj[i].logo}")`;
-    catchphrasediv[0].innerHTML = localVar.cloudobject.busyobj[i].catchphrase;
+    busytitlediv[0].innerHTML = localVar.searchResults[i].busyName;
+    mainpicdiv[0].style.backgroundImage = `url("${localVar.searchResults[i].mainImage}")`;
+    busylogo[0].style.backgroundImage = `url("${localVar.searchResults[i].logo}")`;
+    catchphrasediv[0].innerHTML = localVar.searchResults[i].catchphrase;
     //bionamediv[0].innerHTML = localVar.cloudobject.busyobj[i].alumName;
     //classofdiv[0].innerHTML = "Class Of "+localVar.cloudobject.busyobj[i].alumYear;
 
+    removeClickEventFunc(tempclone,i);
     addClickEventFunc(tempclone,i);
 
     
@@ -260,7 +261,7 @@ function showHideType() {
   let searchButton = document.getElementById("myfancysearchbut");
   let fancyContainer = document.querySelectorAll(".onethirdfancy");
   let alumniContainer = document.getElementById("alumni-type-select");
-  let mySearchQuery = document.querySelectorAll(".mysearchquery")[0];
+
   fancyContainer = fancyContainer[0];
   companyTypesSelect.remove();
   genericTextArea.remove();
@@ -279,20 +280,20 @@ function showHideType() {
       fancyContainer.appendChild(genericTextArea) ;
       fancyContainer.appendChild(searchTypeSelect) ;
       fancyContainer.appendChild(searchButton) ;
-      mySearchQuery.innerHTML = "loii";
+      
 
     }else if(value ==="alumni"){
       fancyContainer.appendChild(alumniContainer) ;
       fancyContainer.appendChild(searchTypeSelect) ;
       fancyContainer.appendChild(searchButton) ;
-      mySearchQuery.innerHTML = "loisssi";
+     
 
     }else if(value ==="type"){
 
       fancyContainer.appendChild(companyTypesSelect) ;
       fancyContainer.appendChild(searchTypeSelect) ;
       fancyContainer.appendChild(searchButton) ;
-      mySearchQuery.innerHTML = "loisoo99i";
+     
 
     }else if(value === "all") {
       let tempDiv = document.createElement("div");
@@ -300,7 +301,7 @@ function showHideType() {
       fancyContainer.appendChild(tempDiv) ;
       fancyContainer.appendChild(searchTypeSelect) ;
       fancyContainer.appendChild(searchButton) ;
-      mySearchQuery.innerHTML = "loiidnnndjjj,";
+      
     }else {
       fancyContainer.innerHTML = "nnnn";
     }
@@ -311,6 +312,278 @@ function showHideType() {
 }
 
 
+function populateSearchResults(context) {
+
+  if(context==="all"){
+
+    addAllToSearchResults();
+    displayWithContext(context);
+
+  }else if(context === "type"){
+
+    addTypeToSearchResults();
+    displayWithContext(context);
+
+  }else if(context === "name"){
+
+    addTypeToSearchResults();
+    displayWithContext(context);
+
+
+  }else if(context === "alumni"){
+
+    addAlumniToSearchResults();
+    displayWithContext(context);
+
+  }else{
+    
+    
+    addAllToSearchResults();
+    displayWithContext("all");
+
+  }
+
+
+}
 
 
 
+function addAllToSearchResults() {
+
+  localVar.searchResults = [];
+
+  for(let i=0; i < localVar.lastRow-1 ; i++){
+    localVar.searchResults[i]= localVar.cloudobject.busyobj[i+1];
+  }
+
+
+}
+
+
+
+
+function addTypeToSearchResults() {
+
+  localVar.searchResults = [];
+
+  
+  let currentSearchTag = localVar.searchKey.value;
+
+  for(let i=0; i < localVar.lastRow ; i++){
+   //
+
+   
+   checkTypeTag(localVar.cloudobject.busyobj[i],currentSearchTag);
+        
+  }
+
+
+}
+
+function addAlumniToSearchResults() {
+
+  localVar.searchResults = [];
+  
+
+  for(let i=0; i < localVar.lastRow ; i++){
+
+    if(localVar.cloudobject.busyobj[i].alumStatus === "yes"){
+      localVar.searchResults.push(localVar.cloudobject.busyobj[i]);
+    }
+        
+  }
+
+
+}
+
+
+
+function displayWithContext(context) {
+
+  if(context==="all"){
+
+    let boxes = localVar.searchResults.length;
+   populatingBoxes(boxes,cardDiv);
+
+  }else if(context==="type"){
+    let boxes = localVar.searchResults.length;
+   
+   populatingBoxes(boxes,cardDiv);
+
+  }else if(context ==="name"){
+    let boxes = localVar.searchResults.length;
+    
+    populatingBoxes(boxes,cardDiv);
+
+  }else if(context ==="alumni"){
+    let boxes = localVar.searchResults.length;
+    
+    populatingBoxes(boxes,cardDiv);
+
+  }{
+    mySearchQuery.innerHTML = "";
+  }
+
+}
+
+
+function cardClickFunction (){
+
+  //  hiddenMenu.style.visibility === "visible";
+
+     
+        hiddenMenu.style.visibility = "visible";
+
+    
+       // title1.innerHTML = this.id;
+
+       let elementId = this.id;
+       let elementIndex = elementId.charAt(elementId.length-1);
+       let i = parseInt(elementIndex,10);
+       i=i+1;
+
+       
+
+        title1.innerHTML = localVar.cloudobject.busyobj[i].busyName;
+        description1.innerHTML = localVar.cloudobject.busyobj[i].busyDescription;
+        email.innerHTML = localVar.cloudobject.busyobj[i].email;
+        phoneone.innerHTML = localVar.cloudobject.busyobj[i].phoneOne;
+        phonetwo.innerHTML = localVar.cloudobject.busyobj[i].phoneTwo;
+        phonethree.innerHTML = localVar.cloudobject.busyobj[i].phoneThree;
+        map.innerHTML = localVar.cloudobject.busyobj[i].mapOne;
+        title2.innerHTML = localVar.cloudobject.busyobj[i].alumName;
+        description2.innerHTML = localVar.cloudobject.busyobj[i].alumBio;
+
+       
+
+      // description1.innerHTML = i;
+        
+    
+}
+
+function searchButStuff () {
+
+  mySearchBut.addEventListener("click", function() {
+     
+    fetchSearchKey();
+    
+    populateSearchResults(localVar.searchKey.type);
+
+    populateSearchKey();
+
+    
+    
+
+  } )
+}
+
+
+
+function fetchSearchKey() {
+    
+  localVar.searchKey.type = document.getElementById("search-type-select").value;
+  
+
+  if(localVar.searchKey.type==="name"){
+
+    localVar.searchKey.value = document.getElementById("myfancykey").value;
+
+  }else if(localVar.searchKey.type==="type"){
+
+    
+    let myTempSelect = document.getElementById("company-type-select");
+    localVar.searchKey.value = myTempSelect.value;
+    localVar.searchKey.textVal = myTempSelect.options[myTempSelect.selectedIndex].text;
+
+  }else if(localVar.searchKey.type==="alumni"){
+
+    localVar.searchKey.value = document.getElementById("alumni-type-select").value;
+
+  }else{
+
+    localVar.searchKey.value = "all";
+  }
+
+  
+
+}
+
+
+
+
+
+function checkTypeTag(itemObject,tag) {
+
+  let thisItemTagsArray = itemObject.tags;
+
+
+  itemObject.searchScore = 0;
+
+  tag = tag.toLowerCase();
+  tag = tag.trim();
+  tag = tag.toString();
+
+
+  for(let i6969=0 ; i6969<thisItemTagsArray.length ; i6969++) {
+    let thisItemThisTagString = thisItemTagsArray[i6969].toString();
+
+    if(tag===thisItemThisTagString){
+      itemObject.searchScore = 10;
+      localVar.searchResults.push(itemObject);
+      break;
+     // let temp306942 = document.createElement("div");
+      //temp306942.innerText = tag+":::"+thisItemThisTagString
+      //mySearchQuery.appendChild(temp306942);
+    
+    }else{
+
+      itemObject.searchScore = 0;
+      
+    }
+  }
+
+  
+
+  return itemObject.searchScore;
+
+
+}
+
+
+function populateSearchKey() {
+
+  let searchValue = "holla";
+  let numOfResults = 0;
+  let searchType = localVar.searchKey.type;
+  
+
+
+  if(searchType === "alumni"){
+
+    searchValue = "IST Alumni Work Here!!";
+
+  }else if (searchType === "type"){
+
+    searchValue = localVar.searchKey.textVal;
+
+  }else if (searchType === "name"){
+
+    searchValue = localVar.searchKey.value;
+
+  }else {
+    
+    
+    searchValue = "All Businesses";
+  }
+
+
+
+
+
+numOfResults = localVar.searchResults.length;
+
+
+  let searchInnerHtml =`Search results for : <span>${searchValue}</span><br>Number of results : <span style="color: #76bc43 !important">${numOfResults}</span>`;
+  mySearchQuery.innerHTML = searchInnerHtml;
+
+}
